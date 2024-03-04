5f58d8b170d2f883860ff99950a65978d0f77c69
mingming <jimin23623@gmail.com>
2024-03-04 14:59:31 +0900

    🧹 eslint 설정 변경

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,13 +4,12 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react', 'react-refresh', 'react-hooks', 'jsx-a11y', 'prettier', 'import'],
   rules: {
     'react/no-unknown-property': ['error', { ignore: ['css'] }], // 알려지지 않은 prop을 사용하지 않도록 해줌 css 제외
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
     'react/react-in-jsx-scope': 'off', // import React from 'react'; 없어도 됨
     'react-refresh/only-export-components': 'off', // 하나의 컴포넌트를 export할 때에만 효과가 있으니 일단 끈다.
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
 };
