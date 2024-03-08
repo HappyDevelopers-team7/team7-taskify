6e1d489ff1c30d750099079d66dc45282f89eabb
mingming <jimin23623@gmail.com>
2024-03-08 17:14:26 +0900

    🧹 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+      { find: '@routes', replacement: path.resolve(__dirname, 'src/routes') },
+      { find: '@hooks', replacement: path.resolve(__dirname, 'src/hooks') },
+      { find: '@styles', replacement: path.resolve(__dirname, 'src/styles') },
+      { find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
+      { find: '@constants', replacement: path.resolve(__dirname, 'src/constants') },
+      { find: '@api', replacement: path.resolve(__dirname, 'src/api') },
+    ],
+  },
 });
