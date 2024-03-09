2bde7eb8432fd5bde4b275bb6517128085ec481a
mingming <jimin23623@gmail.com>
2024-03-09 19:54:26 +0900

    ✨ 로그인 form에 에러가 없다면 버튼 활성화 시킬 수 있도록

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -2,8 +2,34 @@ import { Link } from 'react-router-dom';
 import StSignInContainer from './style';
 import AuthInput from '@/components/auth-input';
 import FullButton from '@/components/full-button';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { postSignIn } from '@/api/postSignIn';
+import { emailPattern } from '@/constants/regex';
 
 const SignIn = () => {
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    setError,
+  } = useForm({ mode: 'onBlur', shouldFocusError: true });
+
+  const handleSubmitLogin: SubmitHandler<FieldValues> = async (data) => {
+    const result = await postSignIn(data.email, data.password);
+
+    if (result === 400) {
+      setError('email', {
+        type: 'serverError',
+        message: '이메일을 다시 확인해 주세요.',
+      });
+      setError('password', {
+        type: 'serverError',
+        message: '비밀번호를 다시 확인해 주세요.',
+      });
+      return;
+    }
+  };
+
   return (
     <StSignInContainer>
       <div className='container__box'>
@@ -13,18 +39,35 @@ const SignIn = () => {
           </Link>
         </h1>
         <h3>오늘도 만나서 반가워요!</h3>
-        <form>
-          <AuthInput id='email' name='email' type='email' placeholder='이메일을 입력해 주세요.' label='이메일' />
+        <form onSubmit={handleSubmit(handleSubmitLogin)}>
+          <AuthInput
+            id='email'
+            name='email'
+            type='email'
+            register={register}
+            errors={errors}
+            rules={{
+              required: '이메일을 입력해 주세요.',
+              pattern: emailPattern,
+            }}
+            placeholder='이메일을 입력해 주세요.'
+            label='이메일'
+          />
           <AuthInput
             id='password'
             name='password'
             type='password'
             password
+            register={register}
+            errors={errors}
+            rules={{
+              required: '비밀번호를 입력해 주세요.',
+            }}
             placeholder='비밀번호를 입력해 주세요.'
             label='비밀번호'
           />
           <div className='form__submit-button'>
-            <FullButton disabled>로그인</FullButton>
+            <FullButton disabled={!isValid}>로그인</FullButton>
           </div>
         </form>
         <h5>
