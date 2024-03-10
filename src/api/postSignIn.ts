4c658c153520abb5f433326aa3152107b3c36440
mingming <jimin23623@gmail.com>
2024-03-10 16:24:39 +0900

    🧹 #12 console.log삭제

--- src/api/postSignIn.ts
+++ src/api/postSignIn.ts
@@ -10,7 +10,6 @@ export const postSignIn = async (email: string, password: string) => {
     });
 
     const responseData = await response;
-    console.log(responseData);
     return responseData;
   } catch (e) {
     const error = e as AxiosError;
