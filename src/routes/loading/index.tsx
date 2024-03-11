4580e40e136acdc56449156b6433241b688fb868
mingming <jimin23623@gmail.com>
2024-03-11 20:58:39 +0900

    🧹 주석 추가

--- src/routes/loading/index.tsx
+++ src/routes/loading/index.tsx
@@ -6,6 +6,7 @@ const Loading = () => {
   return (
     /* children 요소가 로딩 완료될 때 까지 suspense안 fallback 요소를 대신 보여준다. = 로딩 */
     <Suspense fallback={<LoadingSpinner />}>
+      {/* TODO: 우선 main 태그 이렇게 주고 헤더 및 snb 작업이 완료되면 수정하자. */}
       <main>
         <Outlet />
       </main>
