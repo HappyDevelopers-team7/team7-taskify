94838c4b1d5c57d03abd6cdffe7ed2fdfd3ec028
mingming <jimin23623@gmail.com>
2024-03-11 14:31:05 +0900

    🧹 주석정리

--- src/hooks/useAsync.ts
+++ src/hooks/useAsync.ts
@@ -1,22 +1,18 @@
 import { AxiosError } from 'axios';
 import { useCallback, useState } from 'react';
 
-/**
- * 로딩과 에러 메시지를 적을 상태 저장
- * getAsyncFunction에 원하는 api 호출 함수가 들어가야한다. 사용 예제는 sign-in 페이지 참고
- * @param getAsyncFunction 비동기 함수를 넣는다.
- * @returns loading, error, result, setAsyncFunction
- */
-
 interface AsyncHookResult<T> {
   loading: boolean;
   error: AxiosError | unknown | null;
   result: T | undefined;
   setAsyncFunction: (...args: unknown[]) => Promise<void>;
 }
+
 /**
  * 로딩과 에러 메시지를 적을 상태 저장
- * getAsyncFunction에 원하는 api 호출 함수가 들어가야한다.
+ * getAsyncFunction에 원하는 api 호출 함수가 들어가야한다. 사용 예제는 sign-in 페이지 참고
+ * @param getAsyncFunction 비동기 함수를 넣는다.
+ * @returns loading, error, result, setAsyncFunction
  */
 export const useAsync = <T>(getAsyncFunction: (...args: unknown[]) => Promise<T>): AsyncHookResult<T> => {
   const [loading, setLoading] = useState(false);
