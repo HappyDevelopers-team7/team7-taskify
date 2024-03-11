1bacbc61f58730f60a4f6d0cd48c08e134d04431
mingming <jimin23623@gmail.com>
2024-03-11 20:25:04 +0900

    🧹 main 태그 추가

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -65,55 +65,57 @@ const SignIn = () => {
   });
 
   return (
-    <StSignInContainer>
-      <div className='container__box'>
-        <h1>
-          <Link to='/'>
-            <img src='assets/image/logos/largeLogo.svg' alt='랜딩 페이지로 이동하려면 클릭' />
-          </Link>
-        </h1>
-        <h3>오늘도 만나서 반가워요!</h3>
-        <form onSubmit={handleSubmit(handleSubmitLogin)}>
-          <AuthInput
-            id='email'
-            name='email'
-            type='email'
-            register={register}
-            errors={errors}
-            rules={{
-              required: AUTH_ERROR_MESSAGES.EMAIL_REQUIRED,
-              pattern: emailPattern,
-            }}
-            placeholder='이메일을 입력해 주세요.'
-            label='이메일'
-          />
-          <AuthInput
-            id='password'
-            name='password'
-            type='password'
-            password
-            register={register}
-            errors={errors}
-            rules={{
-              required: AUTH_ERROR_MESSAGES.PASSWORD_REQUIRED,
-              pattern: passwordPattern,
-            }}
-            placeholder='비밀번호를 입력해 주세요.'
-            label='비밀번호'
-          />
-          <div className='form__submit-button'>
-            {loading ? (
-              <FullButton disabled={true}>잠시만 기다려주세요...</FullButton>
-            ) : (
-              <FullButton disabled={!isValid}>로그인</FullButton>
-            )}
-          </div>
-        </form>
-        <h5>
-          회원이 아니신가요? <Link to='/sign-up'>회원가입하기</Link>
-        </h5>
-      </div>
-    </StSignInContainer>
+    <main>
+      <StSignInContainer>
+        <div className='container__box'>
+          <h1>
+            <Link to='/'>
+              <img src='assets/image/logos/largeLogo.svg' alt='랜딩 페이지로 이동하려면 클릭' />
+            </Link>
+          </h1>
+          <h3>오늘도 만나서 반가워요!</h3>
+          <form onSubmit={handleSubmit(handleSubmitLogin)}>
+            <AuthInput
+              id='email'
+              name='email'
+              type='email'
+              register={register}
+              errors={errors}
+              rules={{
+                required: AUTH_ERROR_MESSAGES.EMAIL_REQUIRED,
+                pattern: emailPattern,
+              }}
+              placeholder='이메일을 입력해 주세요.'
+              label='이메일'
+            />
+            <AuthInput
+              id='password'
+              name='password'
+              type='password'
+              password
+              register={register}
+              errors={errors}
+              rules={{
+                required: AUTH_ERROR_MESSAGES.PASSWORD_REQUIRED,
+                pattern: passwordPattern,
+              }}
+              placeholder='비밀번호를 입력해 주세요.'
+              label='비밀번호'
+            />
+            <div className='form__submit-button'>
+              {loading ? (
+                <FullButton disabled={true}>잠시만 기다려주세요...</FullButton>
+              ) : (
+                <FullButton disabled={!isValid}>로그인</FullButton>
+              )}
+            </div>
+          </form>
+          <h5>
+            회원이 아니신가요? <Link to='/sign-up'>회원가입하기</Link>
+          </h5>
+        </div>
+      </StSignInContainer>
+    </main>
   );
 };
 
--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -48,97 +48,99 @@ const SignUp = () => {
   }, [result, navigate, setError]);
 
   return (
-    <StSignInContainer>
-      <div className='container__box'>
-        <h1>
-          <Link to='/'>
-            <img src='assets/image/logos/largeLogo.svg' alt='랜딩 페이지로 이동하려면 클릭' />
-          </Link>
-        </h1>
-        <h3>첫 방문을 환영합니다!</h3>
-        <form onSubmit={handleSubmit(handleSubmitRegister)}>
-          <AuthInput
-            id='email'
-            name='email'
-            type='email'
-            register={register}
-            errors={errors}
-            rules={{
-              required: AUTH_ERROR_MESSAGES.EMAIL_REQUIRED,
-              pattern: emailPattern,
-            }}
-            placeholder='이메일을 입력해 주세요.'
-            label='이메일'
-          />
-          <AuthInput
-            id='nickname'
-            name='nickname'
-            type='text'
-            register={register}
-            errors={errors}
-            rules={{
-              required: AUTH_ERROR_MESSAGES.NICKNAME_REQUIRED,
-              pattern: nicknamePattern,
-            }}
-            placeholder='닉네임을 입력해 주세요.'
-            label='닉네임'
-          />
-          <AuthInput
-            id='password'
-            name='password'
-            type='password'
-            password
-            register={register}
-            errors={errors}
-            rules={{
-              required: AUTH_ERROR_MESSAGES.PASSWORD_REQUIRED,
-              pattern: passwordPattern,
-            }}
-            placeholder='8자 이상 입력해 주세요'
-            label='비밀번호'
-          />
-          <AuthInput
-            id='passwordConfirm'
-            name='passwordConfirm'
-            type='password'
-            password
-            register={register}
-            errors={errors}
-            rules={{
-              required: AUTH_ERROR_MESSAGES.CONFIRM_PASSWORD_REQUIRED,
-              validate: {
-                matchPassword: (value) => {
-                  const { password } = getValues();
-                  return password === value || AUTH_ERROR_MESSAGES.INVALID_CONFIRM_PASSWORD;
-                },
-              },
-            }}
-            placeholder='비밀번호를 한번 더 입력해 주세요'
-            label='비밀번호 확인'
-          />
-          <div className='form__agreement-checkbox'>
-            <input
-              type='checkbox'
-              name='agree'
-              id='agree'
-              checked={agreeChecked}
-              onChange={handleChangeActivateRegistration}
+    <main>
+      <StSignInContainer>
+        <div className='container__box'>
+          <h1>
+            <Link to='/'>
+              <img src='assets/image/logos/largeLogo.svg' alt='랜딩 페이지로 이동하려면 클릭' />
+            </Link>
+          </h1>
+          <h3>첫 방문을 환영합니다!</h3>
+          <form onSubmit={handleSubmit(handleSubmitRegister)}>
+            <AuthInput
+              id='email'
+              name='email'
+              type='email'
+              register={register}
+              errors={errors}
+              rules={{
+                required: AUTH_ERROR_MESSAGES.EMAIL_REQUIRED,
+                pattern: emailPattern,
+              }}
+              placeholder='이메일을 입력해 주세요.'
+              label='이메일'
             />
-            <label htmlFor='agree'>이용약관에 동의합니다.</label>
-          </div>
-          <div className='form__submit-button'>
-            {loading ? (
-              <FullButton disabled={true}>잠시만 기다려주세요...</FullButton>
-            ) : (
-              <FullButton disabled={agreeChecked && isValid ? false : true}>가입하기</FullButton>
-            )}
-          </div>
-        </form>
-        <h5>
-          이미 가입하셨나요? <Link to='/sign-in'>로그인하기</Link>
-        </h5>
-      </div>
-    </StSignInContainer>
+            <AuthInput
+              id='nickname'
+              name='nickname'
+              type='text'
+              register={register}
+              errors={errors}
+              rules={{
+                required: AUTH_ERROR_MESSAGES.NICKNAME_REQUIRED,
+                pattern: nicknamePattern,
+              }}
+              placeholder='닉네임을 입력해 주세요.'
+              label='닉네임'
+            />
+            <AuthInput
+              id='password'
+              name='password'
+              type='password'
+              password
+              register={register}
+              errors={errors}
+              rules={{
+                required: AUTH_ERROR_MESSAGES.PASSWORD_REQUIRED,
+                pattern: passwordPattern,
+              }}
+              placeholder='8자 이상 입력해 주세요'
+              label='비밀번호'
+            />
+            <AuthInput
+              id='passwordConfirm'
+              name='passwordConfirm'
+              type='password'
+              password
+              register={register}
+              errors={errors}
+              rules={{
+                required: AUTH_ERROR_MESSAGES.CONFIRM_PASSWORD_REQUIRED,
+                validate: {
+                  matchPassword: (value) => {
+                    const { password } = getValues();
+                    return password === value || AUTH_ERROR_MESSAGES.INVALID_CONFIRM_PASSWORD;
+                  },
+                },
+              }}
+              placeholder='비밀번호를 한번 더 입력해 주세요'
+              label='비밀번호 확인'
+            />
+            <div className='form__agreement-checkbox'>
+              <input
+                type='checkbox'
+                name='agree'
+                id='agree'
+                checked={agreeChecked}
+                onChange={handleChangeActivateRegistration}
+              />
+              <label htmlFor='agree'>이용약관에 동의합니다.</label>
+            </div>
+            <div className='form__submit-button'>
+              {loading ? (
+                <FullButton disabled={true}>잠시만 기다려주세요...</FullButton>
+              ) : (
+                <FullButton disabled={agreeChecked && isValid ? false : true}>가입하기</FullButton>
+              )}
+            </div>
+          </form>
+          <h5>
+            이미 가입하셨나요? <Link to='/sign-in'>로그인하기</Link>
+          </h5>
+        </div>
+      </StSignInContainer>
+    </main>
   );
 };
 
