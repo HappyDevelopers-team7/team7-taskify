4fa97ee14140565878d0564d679a79a72b12e99a
mingming <jimin23623@gmail.com>
2024-03-12 20:23:58 +0900

    🧹 레이아웃 관련 스타일 자잘 수정

--- src/routes/layout/index.tsx
+++ src/routes/layout/index.tsx
@@ -1,15 +1,19 @@
 import DashboardHeader from '@/components/dashboardHeader';
 import SideMenu from '@/components/side-menu';
 import { Outlet } from 'react-router-dom';
+import StWrapper from './style';
 
 const Layout = () => {
   return (
     <>
       <DashboardHeader />
       <main>
-        <SideMenu />
-        <Outlet />
-        <h1>layout</h1>
+        <StWrapper>
+          <SideMenu />
+          <div id='container'>
+            <Outlet />
+          </div>
+        </StWrapper>
       </main>
     </>
   );
---
+++ src/routes/layout/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const StWrapper = styled.div`
+  display: flex;
+  height: 100vh;
+
+  #container {
+    padding-top: 70px;
+  }
+`;
+
+export default StWrapper;
