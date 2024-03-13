f447e88bdc2e229ce466f4988befcf22a756cd7f
jjunhyuki <yjs99004@naver.com>
2024-03-14 01:14:27 +0900

    ⚡️ 대시보드id 와 크라운 띄워봄, 로그인 정보 띄우는건 진행중

--- src/components/dashboardHeader/index.tsx
+++ src/components/dashboardHeader/index.tsx
@@ -1,5 +1,9 @@
 import Container from './style';
-// import { Dashboards } from '@/pages/dashboard';
+import { Dashboards } from '../side-menu';
+// import { useSelector } from 'react-redux';
+// import { getMyInfo, SetMyInfo } from '@/redux/myInfoSlice';
+import { SetMyInfo } from '@/redux/myInfoSlice';
+// import { useEffect, useState } from 'react';
 
 // interface User {
 //   id: number;
@@ -10,34 +14,115 @@ import Container from './style';
 //   updatedAt: string;
 // }
 
-// interface Member extends User { //나중에 멤버들 보여줄떄 사용
-//   userId: number;
-//   isOwner: boolean;
-// }
+interface Props {
+  currentDashboard?: Dashboards;
+}
 
-// interface Props {
-//   currentDashboard?: Dashboards;
-//   user: User | null;
-// }
+// const dashboardHeader = ({ currentDashboard }: Props) => {
+// 임시
+const dashboardHeader = () => {
+  const currentDashboard = {
+    id: 0,
+    title: '대시보드일까?',
+    color: 'string',
+    createdAt: '2024-03-13T13:09:30.708Z',
+    updatedAt: '2024-03-13T13:09:30.708Z',
+    createdByMe: true,
+    userId: 0,
+  };
 
-// const DashboardHeader = ({ currentDashboard, user = null }: Props) => {
-const DashboardHeader = () => {
-  // const title = currentDashboard ? currentDashboard?.title : '내 대시보드';
-  // const showIconClass = 'showIcon';
-  // const hiddenIconClass = 'hiddenIcon';
-  // let showIcon = currentDashboard?.createdByMe ? showIconClass : hiddenIconClass;
+  //임시
+  const myInfo = {
+    id: 0,
+    email: 'jyp1@jyp.com',
+    nickname: '준용용',
+    profileImageUrl: null,
+    createdAt: '2024-03-13T13:09:30.708Z',
+    updatedAt: '2024-03-13T13:09:30.708Z',
+  };
 
+  //전역에서 내 로그인정보 가져옴
+  // const myInfo = useSelector(getMyInfo);
+
+  // const [myInfo, setMyInfo] = useState<User | null>(null);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const res = await getMyInfo();
+  //       if (res) {
+  //         setMyInfo(res.data);
+  //       } else {
+  //         console.log('profile정보를 가져오지 못했습니다.');
+  //       }
+  //     } catch (error) {
+  //       console.log('profile정보를 가져오지 못했습니다.');
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
+  console.log(myInfo.nickname);
   return (
     <Container>
-      <h1>테스트 용 입니다 하하하</h1>
-      {/* <div className='titlebox'>
-        {title} */}
-      {/* className={dashboardInfo?.createdByMe ? 'inline flex-shrink-0' : 'hidden'} */}
-      {/* <img src='assets/image/icons/crownlcon.svg' className={showIcon} alt='crown-icon' />
-      </div> */}
-      {/* <ProfileInfo /> */}
+      <DashboardId currentDashboard={currentDashboard} />
+      <ProfileInfo myInfo={myInfo} />
     </Container>
   );
 };
 
-export default DashboardHeader;
+export default dashboardHeader;
+
+function DashboardId({ currentDashboard }: Props) {
+  const showIconClass = 'showIcon';
+  const hiddenIconClass = 'hiddenIcon';
+  const title = currentDashboard ? currentDashboard?.title : '내 대시보드';
+  const showIcon = currentDashboard?.createdByMe ? showIconClass : hiddenIconClass;
+
+  return (
+    <div className='titlebox'>
+      <div>{title}</div>
+      <img src='/assets/image/icons/crownIcon.svg' className={showIcon} alt='crown-icon' />
+    </div>
+  );
+}
+
+function ProfileInfo({ myInfo }: { myInfo: SetMyInfo | null }) {
+  //프로필 배경색 정해주기
+
+  const COLORS = ['green', 'purple', 'orange', 'blue', 'pink'] as const;
+  type COLORS_TYPE = 'green' | 'purple' | 'orange' | 'blue' | 'pink';
+
+  const generateColor = (name: string): COLORS_TYPE => {
+    const key = name[0].toUpperCase();
+
+    switch (true) {
+      case (key >= 'A' && key < 'F') || (key >= '가' && key < '다'):
+        return COLORS[0];
+      case (key >= 'F' && key < 'K') || (key >= '다' && key < '바'):
+        return COLORS[1];
+      case (key >= 'K' && key < 'Q') || (key >= '바' && key < '아'):
+        return COLORS[2];
+      case (key >= 'Q' && key < 'V') || (key >= '아' && key < '타'):
+        return COLORS[3];
+      default:
+        return COLORS[4];
+    }
+  };
+
+  const initial = myInfo!.nickname[0].toUpperCase();
+
+  let imageBackgroundColor: string = '';
+  if (myInfo && myInfo.profileImageUrl == null) {
+    imageBackgroundColor = generateColor(myInfo.nickname);
+  }
+
+  return (
+    //가져온 정보들을 가지고 여기서 프로필을 띄운다.
+    <div className='myinfo'>
+      <div className={`myinfo-color myinfo-color-${imageBackgroundColor}`}>
+        {/* 이름 첫글자 딴 거 나오게하는 컴포넌트 넣기 */}
+        <div>{initial}</div>
+      </div>
+      <div className='myinfo-name'>{myInfo?.nickname}</div>
+    </div>
+  );
+}
--- src/components/dashboardHeader/style.ts
+++ src/components/dashboardHeader/style.ts
@@ -1,22 +1,59 @@
 import styled from 'styled-components';
 
-const Container = styled.header`
-  position: absolute;
-  top: 0;
-  left: 0;
+const Container = styled.div`
   padding-left: 300px;
   width: 100%;
   height: 70px;
   border-bottom: 1px solid ${({ theme }) => theme.color.gray_d9};
+  flex-shrink: 0;
+  display: flex;
 
   .titlebox {
-    .showicon {
+    width: 70px;
+    height: 24px;
+    display: flex;
+    color: var(--black-black_333236, #333236);
+    font-family: Pretendard;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    padding: 23px 1488px 23px 40px;
+
+    .showIcon {
+      margin: 4px 0px 4px 8px;
       width: 20.103px;
       height: 16px;
       flex-shrink: 0;
     }
-    .hiddenicon {
+    .hiddenIcon {
       visibility: hidden;
+    }
+  }
+
+  .myinfo {
+    display: flex;
+
+    .myinfo-color {
+      width: 38px;
+      height: 38px;
+      flex-shrink: 0;
+    }
+
+    .myinfo-color-green {
+      background-color: #5be352;
+    }
+    .myinfo-color-purple {
+      background-color: #bc57ff;
+    }
+    .myinfo-color-orange {
+      background-color: #ffc85a;
+    }
+    .myinfo-color-blue {
+      background-color: #9dd7ed;
+    }
+    .myinfo-color-pink {
+      background-color: #ff6ee0;
     }
   }
 `;
