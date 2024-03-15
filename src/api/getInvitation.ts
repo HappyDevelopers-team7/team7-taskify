3249c6ba91df9acb3de10c34df9aae8bc70f0494
mingming <jimin23623@gmail.com>
2024-03-16 00:34:38 +0900

    ✨ 초대 리스트 10개 이상일 때 무한스크롤 되도록 (근데 깜빡임)

--- src/api/getInvitation.ts
+++ src/api/getInvitation.ts
@@ -2,14 +2,15 @@ import { AxiosError } from 'axios';
 import API from './constants';
 import axiosInstance from './instance/axiosInstance';
 
-export const getInvitation = async () => {
+export const getInvitation = async (size = 10) => {
   try {
-    const response = await axiosInstance.get(API.INVITATIONS.INVITATIONS);
+    const response = await axiosInstance.get(`${API.INVITATIONS.INVITATIONS}?size=${size}`);
 
     const responseData = await response.data;
     return responseData;
   } catch (e) {
     const error = e as AxiosError;
+    console.log;
     return error.response;
   }
 };
