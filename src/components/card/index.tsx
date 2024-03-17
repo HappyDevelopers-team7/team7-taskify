fe638a46a716de25a951ffd7b8ab8069291698ae
Park JunYong <dyden119@gmail.com>
2024-03-17 23:23:55 +0900

    ✨ 컬럼이 가지고 있는 카드 출력

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from 'react';
 import CardContainer from './style';
+import TagComponent from '../tag-component';
+import randomHexCode from '@/utils/randomHexCode';
 
 interface Props {
   card: {
@@ -17,12 +20,52 @@ interface Props {
   };
 }
 
+interface TagColor {
+  [key: string]: string;
+}
+
 const Card = ({ card }: Props) => {
+  const [tagColors, setTagColors] = useState<TagColor>({});
+
+  useEffect(() => {
+    const createdTagColors: TagColor = {};
+    card.tags.forEach((tag) => {
+      createdTagColors[tag] = randomHexCode();
+    });
+    setTagColors(createdTagColors);
+  }, [card.tags]);
+
+  console.log(card);
   return (
     <CardContainer>
-      {card.imageUrl !== null && <img src={card.imageUrl} className='card-image' alt='card-image' />}
-      <h2>{card.title}</h2>
-      {}
+      {card.imageUrl !== null && <img src={card.imageUrl} className='image-box' alt='card-image' />}
+      <h2 className='title-box'>{card.title}</h2>
+      <div className='tag-box'>
+        {card.tags.length > 0 &&
+          card.tags.map((tag) => (
+            <TagComponent
+              key={card.tags.indexOf(tag)}
+              id={card.tags.indexOf(tag)}
+              name={tag}
+              backgroundColor={tagColors[tag]}
+            />
+          ))}
+      </div>
+      {card.dueDate && (
+        <div className='date-box'>
+          <img src='/assets/image/icons/calendarIcon.svg' />
+          <span>{card.dueDate}</span>
+        </div>
+      )}
+      {card.assignee && (
+        <img
+          className='asignee-box'
+          src={
+            card.assignee.profileImageUrl ? card.assignee.profileImageUrl : '/assets/image/icons/bannerLogoIconXL.svg'
+          }
+          alt='user-image'
+        />
+      )}
     </CardContainer>
   );
 };
--- src/components/card/style.ts
+++ src/components/card/style.ts
@@ -4,14 +4,46 @@ const CardContainer = styled.article`
   width: 314px;
   margin-top: 16px;
   padding: 20px;
+  position: relative;
 
   border-radius: 6px;
   border: 1px solid ${({ theme }) => theme.color.gray_d9};
   background: ${({ theme }) => theme.color.white};
 
-  .card-image {
+  cursor: pointer;
+
+  .image-box {
     width: 274px;
     height: 160px;
+    margin-bottom: 12px;
+  }
+
+  .title-box {
+    font-size: 1.6rem;
+    margin-bottom: 10px;
+  }
+
+  .tag-box {
+    display: flex;
+    gap: 6px;
+    flex-wrap: wrap;
+    margin-bottom: 15px;
+  }
+
+  .date-box {
+    display: flex;
+    align-items: center;
+    gap: 6px;
+    font-size: 1.2rem;
+  }
+
+  .asignee-box {
+    width: 24px;
+    height: 24px;
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+    border-radius: 50%;
   }
 `;
 
