2fbfd3cb184aa2ca59bd8e027325127b52f7c8e0
mingming <jimin23623@gmail.com>
2024-03-17 19:38:34 +0900

    🧹 profile 이미지 수정

--- src/components/profile-image/index.tsx
+++ src/components/profile-image/index.tsx
@@ -3,12 +3,14 @@ import StProfileImage from './style';
 
 export interface ProfileImageProps {
   imageUrl: string | null;
+  size?: number;
+  mobileSize?: number;
   alt?: string;
 }
 
-const ProfileImage = ({ imageUrl, alt }: ProfileImageProps) => {
+const ProfileImage = ({ imageUrl, alt, size, mobileSize }: ProfileImageProps) => {
   return (
-    <StProfileImage>
+    <StProfileImage $size={size} $mobileSize={mobileSize}>
       <img src={getDefaultImageUrlIfNull(imageUrl)} alt={alt ? alt : '프로필 이미지'} />
     </StProfileImage>
   );
