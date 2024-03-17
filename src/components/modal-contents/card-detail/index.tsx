b7c748e762a72dc14218a5080de57ad3497a50bd
mingming <jimin23623@gmail.com>
2024-03-17 15:48:34 +0900

    🧹 column tag name 컴포넌트화

--- src/components/modal-contents/card-detail/index.tsx
+++ src/components/modal-contents/card-detail/index.tsx
@@ -1,6 +1,8 @@
 import ModalContainer from '@/components/modal-container';
 import { closeModal } from '@/redux/modalSlice';
 import { useDispatch } from 'react-redux';
+import StDetailModalContainer from './style';
+import ColumnNameTag from '@/components/column-name-tag';
 
 const CardDetail = () => {
   const dispatch = useDispatch();
@@ -10,7 +12,16 @@ const CardDetail = () => {
   };
   return (
     <ModalContainer type='detail' title='상세글 제목' modalWidth={730} handleCloseModal={handleCloseCardDetailModal}>
-      <h1>상세</h1>
+      <StDetailModalContainer>
+        <div className='content-area'>
+          <div className='tag-area'>
+            <ColumnNameTag name='To do' />
+          </div>
+          <div className='detail-area'></div>
+          <div className='comment-area'></div>
+        </div>
+        <div className='information-area'></div>
+      </StDetailModalContainer>
     </ModalContainer>
   );
 };
---
+++ src/components/modal-contents/card-detail/style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const StDetailModalContainer = styled.article`
+  display: flex;
+`;
+
+export default StDetailModalContainer;
