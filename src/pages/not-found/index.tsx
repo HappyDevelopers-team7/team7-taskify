44ec4e39aaf8d973a046ee1a815962ffb35cda7f
mingming <jimin23623@gmail.com>
2024-03-21 16:08:12 +0900

    🧹 page not found 작업

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,5 +1,30 @@
+import { Link } from 'react-router-dom';
+import StNotFound from './style';
+
 const NotFound = () => {
-  return <h1>페이지 못찾았어용</h1>;
+  return (
+    <>
+      <StNotFound>
+        <div className='container'>
+          <h1 className='logo-area'>
+            <Link to='/' role='button'>
+              <img src='/assets/image/logos/errorLogo.svg' alt='페이지를 찾을 수 없습니다. 메인으로 이동하기' />
+            </Link>
+          </h1>
+          <div className='text-area'>
+            <h2>페이지를 찾을 수 없습니다 &#58; &#40;</h2>
+            <p>페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다.</p>
+            <p>입력하신 주소를 다시 확인해 주세요.</p>
+          </div>
+          <div className='button-area'>
+            <Link to='/' role='button'>
+              메인으로 돌아가기
+            </Link>
+          </div>
+        </div>
+      </StNotFound>
+    </>
+  );
 };
 
 export default NotFound;
