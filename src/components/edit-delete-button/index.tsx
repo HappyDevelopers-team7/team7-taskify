9d4df543bdff2fbcf22b20d31d4a7cc87dc645a0
jjunhyuki <yjs99004@naver.com>
2024-03-23 08:10:49 +0900

    🎨 대시보드 삭제 컴포넌트 완료

--- src/components/edit-delete-button/index.tsx
+++ src/components/edit-delete-button/index.tsx
@@ -1,15 +1,23 @@
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import StDeleteButton from './style';
 import { deleteDashboard } from '@/api/deleteDashboard';
 
-function EditDeleteButton() {
-  const { dashboardId } = useParams();
+interface EditDeleteButtonProps {
+  dashboardId: string | undefined;
+}
+
+function EditDeleteButton({ dashboardId }: EditDeleteButtonProps) {
   const navigate = useNavigate();
 
   const handleDelete = async () => {
+    console.log(dashboardId);
     if (dashboardId !== undefined) {
-      deleteDashboard(dashboardId);
-      navigate('/');
+      try {
+        await deleteDashboard(dashboardId);
+        navigate('/');
+      } catch (error) {
+        console.error('대시보드 삭제 중 오류 발생:', error);
+      }
     }
   };
   return <StDeleteButton onClick={handleDelete}>대시보드 삭제하기</StDeleteButton>;
