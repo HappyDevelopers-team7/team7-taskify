6ec4ae6f5a0cdc63a7f8d05727520a0a66178226
mingming <jimin23623@gmail.com>
2024-04-01 12:19:09 +0900

    ♻️ comment post error 처리 수정

--- src/api/postComment.ts
+++ src/api/postComment.ts
@@ -1,6 +1,12 @@
 import { AxiosError } from 'axios';
 import API from './constants';
 import axiosInstance from './instance/axiosInstance';
+import { COMMENT_ERROR_MESSAGES } from '@/constants/message';
+import { toast } from 'react-toastify';
+
+interface CustomErrorData {
+  message: string;
+}
 
 export const postComment = async (content: string, columnId: number, cardId: number, dashboardId: number) => {
   try {
@@ -11,11 +17,15 @@ export const postComment = async (content: string, columnId: number, cardId: num
       dashboardId,
     });
 
-    const responseData = await response.data;
-
-    return responseData;
+    return response.data;
   } catch (e) {
-    const error = e as AxiosError;
-    return error.response;
+    const error = e as AxiosError<CustomErrorData>;
+    const errorStatus = error.response?.status;
+    const errorMessage = error.response?.data?.message;
+    if (errorStatus === 400) {
+      return toast.error(COMMENT_ERROR_MESSAGES.REQUIRE_COMMENT_CONTENT);
+    }
+
+    return toast.error(errorMessage);
   }
 };
